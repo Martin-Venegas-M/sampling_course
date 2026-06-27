1180c12deda6b11e3322713d0481d8753c1b671e
Martin-Venegas-M <martin.venegas@ug.uchile.cl>
2026-06-27 15:53:28 -0400

    Finish quiz

--- assignment3/quiz3.R
+++ assignment3/quiz3.R
@@ -33,6 +33,7 @@ pop <- tibble::tibble(
 # has the characteristic of interest.
 
 p_w <- with(pop, sum(W_h * P_h))
+p_w # 0.4
 
 # 2) Compute the sampling variance of the mean from the proportionately
 # allocated sample of n = 30
@@ -49,22 +50,43 @@ pop <- pop |>
     var_p_h = (1 - n_h / N_h) * ((p_h * (1 - p_h))/(n_h - 1))
     )
 
+pop$var_p_h # 0.03800000 0.02638889 0.01085714
+
+var_p_w_prop_alloc <- with(pop, sum((W_h^2) * var_p_h))
+var_p_w_prop_alloc # 0.00670194
+
 # 3) If S2 = 0.24, what is the simple random sampling variance of the estimated
 # proportion,
 
 S2 <- 0.24
 var_srs <- (1 - f) * (S2/n) 
+var_srs # 0.0076
                 
 # 4) What is the gain in precision from using proportionately allocated stratified
 # sampling?
 
-var_p_w <- with(pop, sum((W_h^2) * var_p_h))
-deff <- var_p_w / var_srs
-precision <- 1 - deff
+deff_prop_alloc <- var_p_w_prop_alloc / var_srs
+precision_prop_alloc <- 1 - deff_prop_alloc
+
+deff_prop_alloc # 0.8818342
+precision_prop_alloc # 0.1181658
 
 # 5) Compute the sampling variance of the mean from the entire “equal
 # allocation” sample of n = 30. That is, compute v(pw)= Σh=1Wh2v(ph ) using the
 # hint in problem 2 when sample sizes are equal to 10 in each stratum.
 
+pop <- pop |> 
+  mutate(
+    n_h_new = rep(10, 3),
+    var_p_h_new = (1 - n_h_new / N_h) * ((p_h * (1 - p_h))/(n_h_new - 1))
+  )
+
+var_p_w_eq_alloc <- with(pop, sum((W_h^2) * var_p_h_new))
+var_p_w_eq_alloc # 0.00767284
+
+
 # 6) What is the design effect from using “equal allocation” stratified sampling? v(p_w)
 # That is, what is deff = v(p_w) / v(p_0)?
+
+deff_eq_alloc <- var_p_w_eq_alloc / var_srs
+deff_eq_alloc # 1.009584
